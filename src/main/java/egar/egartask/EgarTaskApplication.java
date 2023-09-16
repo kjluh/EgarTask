86de1dc0a5dabc101f65db810e9645419da41be6
Anatoly <kjluh@mail.ru>
2023-09-16 09:45:29 +0300

    Initial commit

---
+++ src/main/java/egar/egartask/EgarTaskApplication.java
@@ -0,0 +1,13 @@
+package egar.egartask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgarTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgarTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/egar/egartask/EgarTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package egar.egartask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgarTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
