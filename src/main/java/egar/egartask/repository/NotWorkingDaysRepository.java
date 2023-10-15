18226958a22ef7e368d9e2a46094be19d049d1b2
Anatoly <kjluh@mail.ru>
2023-10-15 16:19:20 +0300

    устранил все кроме нового кода.

--- src/main/java/egar/egartask/repository/NotWorkingDaysRepository.java
+++ src/main/java/egar/egartask/repository/NotWorkingDaysRepository.java
@@ -1,8 +1,10 @@
 package egar.egartask.repository;
 
+import egar.egartask.entites.Consumer;
 import egar.egartask.entites.Employee;
 import egar.egartask.entites.NotWorkingDays;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,19 +22,25 @@ public class NotWorkingDaysRepository {
     @Autowired
     private EntityManager entityManager;
 
-    //save + find
-    public List<NotWorkingDays> getNotWorkingDaysByEmployee_Id(Long id) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<NotWorkingDays> criteriaQuery = criteriaBuilder.createQuery(NotWorkingDays.class);
-        Root<NotWorkingDays> root = criteriaQuery.from(NotWorkingDays.class);
-        Join<NotWorkingDays, Employee> join =  root.join("employee_id");
-        criteriaQuery.select(root);
-
-//        criteriaQuery.where(criteriaBuilder.equal(root.get("employee_id"), id));
+//    public List<NotWorkingDays> getNotWorkingDaysByEmployee_Id(Long id) {
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<NotWorkingDays> criteriaQuery = criteriaBuilder.createQuery(NotWorkingDays.class);
+//        Root<NotWorkingDays> root = criteriaQuery.from(NotWorkingDays.class);
+//        criteriaQuery.select(root);
+//
+//        Join<NotWorkingDays, Employee> join = root.join("id");
+//        Predicate p = criteriaBuilder.equal(join.get("id"), id);
+//
+//        criteriaQuery.where(p);
 //        List<NotWorkingDays> list = entityManager.createQuery(criteriaQuery).getResultList();
-        List<NotWorkingDays> list = entityManager.createQuery(criteriaQuery).getResultList();
-        List<NotWorkingDays> newList = list.stream().filter(e->e.getEmployee().getId()==id).toList();
-        return newList;
+////        List<NotWorkingDays> newList = list.stream().filter(e->e.getEmployee().getId()==id).toList();
+//        return list;
+//    }
+
+    public List<NotWorkingDays> getNotWorkingDaysByEmployee_Id(Long id) {
+        TypedQuery<NotWorkingDays> query = entityManager.createQuery(
+                "select n from NotWorkingDays n where n.employee.id=:id", NotWorkingDays.class);
+        return query.setParameter("id", id).getResultList();
     }
 
     public List<NotWorkingDays> getNotWorkingDaysByComments(String com) {
